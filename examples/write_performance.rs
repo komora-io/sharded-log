0d8d462298eb02dcfe7e3b9384b9d23cf154a39b
Tyler Neely <t@jujit.su>
2022-05-16 15:34:27 +0200

    Add write performance example

---
+++ examples/write_performance.rs
@@ -0,0 +1,76 @@
+use sharded_log::Config;
+use std::sync::{Arc, Barrier};
+
+fn main() {
+    let number_of_threads = 16;
+    let buffers_per_batch = 8;
+    let buffer_size = 512;
+    let batches_per_thread = 128 * 1024;
+
+    let config = Config {
+        path: "write_performance".into(),
+        ..Default::default()
+    };
+
+    let mut recovery = config.recover().unwrap();
+    for _ in &mut recovery {}
+
+    let log = Arc::new(
+        recovery.truncate_logs_and_start_writing().unwrap(),
+    );
+
+    let barrier =
+        Arc::new(Barrier::new(number_of_threads + 1));
+
+    let mut threads = vec![];
+
+    let before = std::time::Instant::now();
+
+    for _ in 0..number_of_threads {
+        let barrier = barrier.clone();
+        let log = log.clone();
+
+        let thread = std::thread::spawn(move || {
+            barrier.wait();
+
+            for _ in 0..batches_per_thread {
+                let mut batch =
+                    Vec::with_capacity(buffers_per_batch);
+                for _ in 0..buffers_per_batch {
+                    let mut buffer =
+                        Vec::with_capacity(buffer_size);
+                    unsafe {
+                        buffer.set_len(buffer_size);
+                    }
+                    batch.push(buffer);
+                }
+                log.write_batch(&batch).unwrap();
+            }
+        });
+
+        threads.push(thread);
+    }
+
+    barrier.wait();
+
+    for thread in threads.into_iter() {
+        thread.join().unwrap();
+    }
+
+    log.flush().unwrap();
+
+    let bytes_written = number_of_threads
+        * batches_per_thread
+        * buffers_per_batch
+        * buffer_size;
+    let throughput = bytes_written as u128
+        / before.elapsed().as_micros();
+
+    println!("wrote {} megabytes per second, {} threads, {} batches per thread, {} buffers per batch, {} bytes per buffer",
+        throughput,
+        number_of_threads,
+        batches_per_thread,
+        buffers_per_batch,
+        buffer_size
+    );
+}
